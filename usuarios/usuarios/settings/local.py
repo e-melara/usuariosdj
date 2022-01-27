cb5dc8fdba5a0507939537936ca2fdf493d89e03
Edwin Melara <edwin.melara.landaverde@gmail.com>
2022-01-27 15:42:37 -0600

    Configurando el proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psyconpg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5433'
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
